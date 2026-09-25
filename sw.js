6d3672e8e9b27d46d4c9ccce0381d1bbafa185af
romaastahov99-sudo <romaastahov99@gmail.com>
2026-09-26 02:02:03 +0700

    bw4

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = "guru-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
+  self.skipWaiting();
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", e => {
+  const url = new URL(e.request.url);
+  if (url.hostname.includes("firebase") || url.hostname.includes("googleapis") || url.hostname.includes("gstatic")) return;
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      if (res.ok && e.request.method === "GET") {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }).catch(() => cached)));
+  }
+});
